fedaa3ec6b7e268bb63294f23ef1bae61c35b4a1
thulasizwe33 <thulasizwe3@gmail.com>
2024-05-01 14:30:00 -0700

    0. Restart from scratch!

---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status')
+def status():
+    """return status in json format"""
+    s = {"status": "OK"}
+    return jsonify(s)
+
+
+@app_views.route('/stats')
+def stats():
+    """ """
+    classes = {"amenities": "Amenity", "cities": "City",
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
+    retval = {}
+    for key, val in classes.items():
+        retval[key] = storage.count(val)
+    return jsonify(retval)
---
+++ api/v1/views/states.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.state import State
+from models import storage
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def getAllStates():
+    """Retrieves the list of all State objects"""
+    retval = []
+    all_states = storage.all('State')
+    for item in all_states.values():
+        retval.append(item.to_dict())
+    return jsonify(retval)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+def GET_state(state_id):
+    """GET State object, else raise 404"""
+
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False, methods=['DELETE'])
+def DEL_state(state_id):
+    """ delete a state object """
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+
+    state.delete()
+    storage.save()
+    storage.close()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def POST_state():
+    """adds state, raise 400 if not valid json"""
+    post_content = request.get_json()
+
+    if not request.is_json:
+        abort(400, "Not a JSON")
+
+    name = post_content.get('name')
+    if not name:
+        abort(400, "Missing name")
+
+# send in user input(key:value) to create new object
+    new_state = State(**post_content)
+    storage.new(new_state)
+
+    new_state.save()
+    storage.close()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def PUT_state(state_id):
+    """ """
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+
+    ignore_keys = ["id", "created_at", "updated_at"]
+
+    content = request.get_json()
+
+    if not request.is_json:
+        abort(400, "Not a JSON")
+
+    for key, val in content.items():
+        if key not in ignore_keys:
+            setattr(state, key, val)
+
+    state.save()
+    storage.close()
+
+    return jsonify(state.to_dict())
